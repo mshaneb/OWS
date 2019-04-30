9a9da4945cc99a88f0e744a3e78cb37e0c319437
matt <bangerpowered@gmail.com>
2019-04-30 15:46:24 -0400

    Building exe

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
-from distutils.core import setup
-import py2exe
+import cx_Freeze as cx_f
+import sys
+import os
 
-setup(console=["__init__.py"])
+base = None
+if (sys.platform == "win32"):
+    base = "Win32GUI"
+
+
+os.environ['TCL_LIBRARY'] = r'D:\Git\mingw64\lib\tcl8.6'
+os.environ['TK_LIBRARY'] = r'D:\Git\mingw64\lib\tk8.6'
+
+executables = [cx_f.Executable("__init__.py")]
+
+cx_f.setup(name = "run", executables=executables)
+
